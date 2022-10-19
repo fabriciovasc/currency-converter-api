df3beb98df601ea3d4df9fcfcc640cf481f90650
Fabrício Vasconcellos <fabricio.vasconc@gmail.com>
2022-10-18 23:17:09 -0300

    Add @dotenv to read .env files and improve start server

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
+import * as dotenv from 'dotenv';
+
 import { Server } from 'net';
 import createServer from './server';
-import appConfig from './config';
+import config from './config';
+
+dotenv.config();
+
+const SERVER_PORT = config.app.port;
 
 const startServer = (): Server => {
   const app = createServer();
 
-  return app.listen(appConfig.app.port, () => {
-    console.log(`Server is listening on port ${appConfig.app.port}`);
+  return app.listen(SERVER_PORT, () => {
+    console.log(`Server is listening on port ${SERVER_PORT}`);
   });
 };
 
