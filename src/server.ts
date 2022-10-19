83e85a7a5e4ccabf839d34151bb9f22d75dfeeef
Fabrício Vasconcellos <fabricio.vasconc@gmail.com>
2022-10-18 23:20:34 -0300

    Add cors options and use json in req formats

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,16 @@
 import express, { Application } from 'express';
+import cors, { CorsOptions } from 'cors';
 
 const createServer = (): Application => {
   const app = express();
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    credentials: false
+  };
+
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+  app.use(cors(corsOptions));
 
   return app;
 };
