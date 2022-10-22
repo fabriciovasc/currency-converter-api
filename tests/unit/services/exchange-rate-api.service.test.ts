8976fb727b7e3f00ccc52f55334a50c74c7ca4f4
Fabrício Vasconcellos <fabricio.vasconc@gmail.com>
2022-10-21 22:26:35 -0300

    Add exchange rate api test

---
+++ tests/unit/services/exchange-rate-api.service.test.ts
@@ -0,0 +1,54 @@
+import ExchangeRateApiService from '../../../src/api/services/exchange-rate-api.service';
+import axios from 'axios';
+import mocked = jest.mocked;
+
+jest.mock('axios');
+jest.mock('@services/exchange-rate-api.service');
+
+const axiosMock = mocked(axios);
+const exchangeRateApiMock = mocked(new ExchangeRateApiService());
+
+beforeEach(() => {
+  axiosMock.mockReset();
+  exchangeRateApiMock.getExchangeRates.mockReset();
+});
+
+describe('exchange rate api service', () => {
+  test('should throw an error because not success response', async () => {
+    // Given
+    const mockedResponseError: any = {
+      success: false,
+      error: {
+        type: 'currency_not_found'
+      }
+    };
+
+    // When
+    axiosMock.get.mockResolvedValue(mockedResponseError);
+    exchangeRateApiMock.getExchangeRates.mockRejectedValue(new Error(mockedResponseError.error.type));
+
+    // Then
+    const exchangeRateApi = new ExchangeRateApiService();
+    await expect(exchangeRateApi.getExchangeRates()).rejects.toThrowError(mockedResponseError.error.type);
+  });
+
+  test('should return exchange rates', async () => {
+    // Given
+    const mockedResponse: any = {
+      success: true,
+      rates: {
+        USD: 1.05,
+        BRL: 5.45,
+        EUR: 9.44
+      }
+    };
+
+    // When
+    axiosMock.get.mockResolvedValue(mockedResponse);
+    exchangeRateApiMock.getExchangeRates.mockResolvedValue(mockedResponse.rates);
+
+    // Then
+    const exchangeRateApi = new ExchangeRateApiService();
+    await expect(exchangeRateApi.getExchangeRates()).resolves.toEqual(mockedResponse.rates);
+  });
+});
