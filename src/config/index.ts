0115fa10ba3cbb0681a6c8aebc51e0b3637a5fdf
Fabrício Vasconcellos <fabricio.vasconc@gmail.com>
2022-10-22 21:07:10 -0300

    Add database config in app config

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,6 +5,9 @@ const appConfig = {
     apiVersion: process.env.API_VERSION || 'v1',
     port: parseInt(<string>process.env.PORT, 10) || 8090
   },
+  database: {
+    url: process.env.DATABASE_URL || 'file:./dev.db'
+  },
   exchangeRateApi: {
     ACCESS_KEY: process.env.EXCHANGE_RATE_ACCESS_KEY || 'CCPa1bY1jjusV0uYEsCN9ZqLNqsSA0IZ'
   }
