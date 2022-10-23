d81af281eb8347b02ab98139d0e734e58543c7b9
Fabrício Vasconcellos <fabricio.vasconc@gmail.com>
2022-10-22 23:15:26 -0300

    Implement logger in error handler and use morgan in http requests

--- src/api/middlewares/handlers/error-handler.middleware.ts
+++ src/api/middlewares/handlers/error-handler.middleware.ts
@@ -1,5 +1,6 @@
 import HttpException from '@models/http-exception.model';
 import { NextFunction, Request, Response } from 'express';
+import Logger from '../../../utils/logger';
 
 interface ResponseType {
   code: number;
@@ -7,22 +8,42 @@ interface ResponseType {
   message: string;
 }
 
+interface ErrorLog extends ResponseType {
+  method: string;
+  path: string;
+  params: string;
+  body: any;
+  query: any;
+}
+
+const generateLogs = (
+  error: ResponseType,
+  { method, path, route: { path: params }, query, body }: Request
+): ErrorLog => {
+  return { ...error, method, path, params, query, body };
+};
+
 function errorHandler(err: HttpException, req: Request, res: Response, next: NextFunction) {
+  let status: number;
+  let response: ResponseType;
+
   if (!err.errorCode) {
-    const unknownResponse: ResponseType = {
+    response = {
       code: 500,
       type: 'UNKNOWN_ERROR',
       message: 'Erro desconhecido'
     };
-    res.status(500).json(unknownResponse);
+    status = 500;
   } else {
     const { errorCode: code, errorType: type, statusCode, message } = err;
-
-    // TODO: Add logger constructor
-
-    const response = { code, type, message };
-    res.status(statusCode).json(response);
+    response = { code, type, message };
+    status = statusCode;
   }
+
+  const logs = generateLogs(response, req);
+  Logger.error(JSON.stringify(logs));
+
+  res.status(status).json(response);
 }
 
 export default errorHandler;
